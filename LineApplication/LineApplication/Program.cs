e640daf54645184bc69213e0d724ebb9716bd6dd
mazhang <maosheng.zhang@gmail.com>
2017-07-30 17:04:38 +0800

    update code.

--- LineApplication/LineApplication/Program.cs
+++ LineApplication/LineApplication/Program.cs
@@ -9,9 +9,14 @@ namespace LineApplication
     class Line
     {
         private double length;
-        public Line()
+        public Line(double len)
         {
             Console.WriteLine("对象已创建！");
+            length = len;
+        }
+        ~Line()
+        {
+            Console.WriteLine("对象已删除！");
         }
         public void setLength(double len)
         {
@@ -26,10 +31,11 @@ namespace LineApplication
     {
         static void Main(string[] args)
         {
-            Line line = new Line();
-            line.setLength(10.0);
+            Line line = new Line(10.0);
             Console.WriteLine("Line's length: {0}", line.getLength());
-            Console.ReadLine();
+            line.setLength(12.0);
+            Console.WriteLine("Line's length: {0}", line.getLength());
+            //Console.ReadLine();
         }
     }
 }
