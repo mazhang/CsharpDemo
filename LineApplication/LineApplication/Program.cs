972389da1fdb072bbfc4021ae992b083d2eae794
mazhang <maosheng.zhang@gmail.com>
2017-07-30 16:54:36 +0800

    update

--- LineApplication/LineApplication/Program.cs
+++ LineApplication/LineApplication/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace LineApplication
 {
+    class Line
+    {
+        private double length;
+        public Line()
+        {
+            Console.WriteLine("对象已创建！");
+        }
+        public void setLength(double len)
+        {
+            length = len;
+        }
+        public double getLength()
+        {
+            return length;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Line line = new Line();
+            line.setLength(10.0);
+            Console.WriteLine("Line's length: {0}", line.getLength());
+            Console.ReadLine();
         }
     }
 }
