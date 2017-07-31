ada514b24a064a3c4542dce472f62138bb4cca64
mazhang <maosheng.zhang@gmail.com>
2017-07-31 22:32:31 +0800

    Create some new projects.

---
+++ InheritanceApplication/InheritanceApplication/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace InheritanceApplication
+{
+    class Shape
+    {
+        protected int width;
+        protected int height;
+        public void SetWidth(int w)
+        {
+            width = w;
+        }
+        public void SetHeight(int h)
+        {
+            height = h;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public int GetArea()
+        {
+            return width * height;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.SetWidth(10);
+            rect.SetHeight(11);
+
+            Console.WriteLine("The area of rect is: {0}", rect.GetArea());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticVar/StaticVar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticVar
+{
+    class StaticVar
+    {
+        //可以使用 static 关键字把类成员定义为静态的。当声明一个类成员为静态时，无论有多少个类的对象被创建，只会有一个该静态成员的副本。
+        public static int num;
+        public void count()
+        {
+            num++;
+        }
+        public int getNum()
+        {
+            return num;
+        }
+    }
+    class StaticVarTest
+    {
+        static void Main(string[] args)
+        {
+            StaticVar s1 = new StaticVar();
+            StaticVar s2 = new StaticVar();
+
+            s1.count();
+            s2.count();
+
+            Console.WriteLine("s1的变量num: {0}", s1.getNum());
+            Console.WriteLine("s2的变量num: {0}", s2.getNum());
+            Console.ReadKey();
+        }
+    }
+}
